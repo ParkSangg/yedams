00d0f2bd165d432a3612dff5c78312d3dbde495f
admin <admin@DESKTOP-JBO3G0H>
2023-05-30 10:43:24 +0900

    Fisrtcommit.

---
+++ src/com/yedam/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.yedam;
+
+public class HelloWorld {
+	// main 메소드 시작.
+	public static void main(String[] args) {
+		System.out.print("Hello,World!");
+		System.out.println("Git 연동");
+	}
+}
